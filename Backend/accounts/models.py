77cc3506fff925f9ec2036d4537ac3084a5ad1f7
Brian Chirchir <bchirchir97@gmail.com>
2023-12-22 02:26:16 +0300

    Update models.py

--- Backend/accounts/models.py
+++ Backend/accounts/models.py
@@ -22,6 +22,7 @@ class CustomUser(AbstractUser):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField(unique=True) 
+    username = models.CharField(max_length=30)
     phone = models.CharField(max_length=15, blank=True, null=True)
 
     objects = CustomUserManager()
